a797df7cebeafcc618c5ccc9cc94eecdf2c4a9a4
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2026-05-26 10:41:02 -0600

    feat(persistence): add base entities -Create core domain entities (Cliente, Categoria). -Add intermediate entity CompraProducto. -Implement composite key CompraProductoPK(@Embeddable and Serializable)

---
+++ src/main/java/mx/tecdesoftware/market_backend/persistence/entity/Cliente.java
@@ -0,0 +1,68 @@
+package mx.tecdesoftware.market_backend.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String celular;
+    @Column (name = "correo_electronico")
+    private String correoElectronico;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
